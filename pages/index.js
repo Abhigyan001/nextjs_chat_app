2f3148e75a4d38f5a19d7cefd0df2015c781e3de
Abhiyan001 <abhigyanmahanta001@gmail.com>
2023-06-06 10:19:52 +0530

    Add netlify live link

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ const Auth = () => {
     <div className="background">
       <div className="auth-container">
         <form className="auth-form" onSubmit={(e) => onSubmit(e)}>
-          <div className="auth-title">NextJS Chat</div>
+          <div className="auth-title">NextJS Chat App</div>
 
           <div className="input-container">
             <input
